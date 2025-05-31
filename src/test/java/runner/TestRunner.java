1a17f35de3dcbbf5e85355fa1b5fbf1d2d53b9ea
Novan Adhagara <novan.adhagara@gmail.com>
2025-05-31 10:01:59 +0700

    commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,23 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.java.BeforeAll;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"defenitions", "hooks"},
+    plugin = {"pretty",                                   // Pretty console output
+                "html:target/cucumber-report.html",          // HTML report
+                "json:target/cucumber-report.json",          // JSON report
+                "junit:target/cucumber-report.xml"},         // JUnit XML report
+        
+    monochrome = true
+
+)
+
+public class TestRunner {
+}
